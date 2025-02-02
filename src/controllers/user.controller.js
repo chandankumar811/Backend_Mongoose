847d148161d64898e19fbc7208f02f449b9ed4f0
Liladhar  Kumar  Kushawaha <wwwck811@gmail.com>
2025-02-03 00:00:04 +0530

    restructure file

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -52,15 +52,20 @@ const registerUser = asyncHandler(async (req, res) => {
     username: username.toLowerCase(),
   });
 
-  const createdUser = await User.findById(user._id).select("-password -refreshToken")
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
 
-  if(!createdUser){
-    throw new ApiError(500, "something went to wrong while registering the user")
+  if (!createdUser) {
+    throw new ApiError(
+      500,
+      "something went to wrong while registering the user"
+    );
   }
 
-  return res.status(201).json(
-    new ApiResponse(200, createdUser, "user register successfully")
-  )
+  return res
+    .status(201)
+    .json(new ApiResponse(200, createdUser, "user register successfully"));
 });
 
 export { registerUser };
